b68816dbb7c3916364a6c25c832b45b890ddbed1
Bernardo D'Angelo <bernardodangelo8@gmail.com>
2024-10-09 20:40:46 -0300

    classe principal criada

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def main_menu():
+    print("Sistema de Controle de Ponto")
+    print("1. Relatórios")
+    print("2. Inserir Registros")
+    print("3. Remover Registros")
+    print("4. Atualizar Registros")
+    print("5. Sair")
+
+while True:
+    main_menu()
+    choice = input("Escolha uma opção: ")
